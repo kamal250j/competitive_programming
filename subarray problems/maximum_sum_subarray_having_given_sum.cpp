8b13b1effef425d2e2695f9a3e6441237dd9654c
kamal250j <kamal250j@gmail.com>
2021-02-27 20:52:02 +0530

    added subarray problem

---
+++ subarray problems/maximum_sum_subarray_having_given_sum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+//#define int long long
+using namespace std;
+signed main() {
+    
+    int n,s;
+    cin>>n>>s;
+    int a[n];
+    int sum = 0;
+    map<int,int> mp;
+    
+    int maxi = 0;
+    mp.clear();
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+        sum += a[i];
+        
+        // Here cannot use if(mp[sum] != 0) becuase if sum is not present then mp[sum] has undefined behaviour
+        // Use this mp[element] if and only if you are sure element will be present in map
+        if(mp.find(sum) == mp.end()) 
+            mp[sum] = i;
+        if(mp.find(sum-s) != mp.end() && (i - mp[sum-s] > maxi))
+           maxi = i - mp[sum-s];
+        
+    }
+    
+    for(map<int,int>::iterator it = mp.begin(); it != mp.end();it++)
+        cout<<it->first<<" "<<it->second<<endl;
+           
+    cout<<maxi<<endl;
+}
