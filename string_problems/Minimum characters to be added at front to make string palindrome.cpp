ed7a6714515775432c940c7508a2a8b1b3b50271
Kamal <kamal250j@gmail.com>
2021-05-15 13:59:30 +0530

    Update Minimum characters to be added at front to make string palindrome.cpp

--- string_problems/Minimum characters to be added at front to make string palindrome.cpp
+++ string_problems/Minimum characters to be added at front to make string palindrome.cpp
@@ -6,51 +6,26 @@
 using namespace std;
 int main()
 {
-    string s;
-    cin>>s;
+    string A;
+    cin>>A;
     
-    int dp[s.length()][s.length()];
-    int len = s.length();
-    memset(dp,0,sizeof(dp));
-    for(int i=0;i<s.length();i++) {
-        dp[i][i] = 1;
-        dp[i][min(i+1,len-1)] = (s[i] == s[i+1]) ? 1 : 0;
-    }
-    
-    int l = 0, r = 0;
-    for(int i=0;i<s.length();i++) {
-        l = i; r = i;
-        while(l >= 0 && r < s.length()) {
-            if(s[l] == s[r]) {
-                dp[l][r] = 1;
-            }
-            else
-                break;
-            l--; r++;
+    string B = A;
+    int lps[2*A.length()+1];
+    memset(lps,0,sizeof(lps));
+    int prev;
+    string rev = A;
+    reverse(rev.begin(),rev.end());
+    A = A + "#" + rev;
+    for(int i=1;i<A.length();i++) {
+        prev = lps[i-1];
+        while(prev > 0 && A[i] != A[prev]) {
+            prev = lps[prev-1];
         }
+        if(A[i] == A[prev]) 
+            prev++;
+        lps[i] = prev;
     }
     
-    for(int i=0;i+1<s.length();i++) {
-        l = i; r = i+1;
-        while(l >= 0 && r < s.length()) {
-            if(s[l] == s[r]) {
-                dp[l][r] = 1;
-            }
-            else
-                break;
-            l--; r++;
-        }
-    }
-    
-    int mn = 0;
-    int ans = -1;
-    for(int j=0;j<s.length();j++) {
-        if(dp[0][j] && mn < j+1) {
-            mn = j+1;
-            ans = s.length()-j-1;
-        }
-    }
-    
-    cout<<ans<<endl;
+    cout<<B.length() - lps[A.length()-1];
     
 }
